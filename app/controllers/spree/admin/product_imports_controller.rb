6f201149e698640ebed46245402ac42f2be58982
Michael Bianco <iloveitaly@gmail.com>
2012-03-08 10:41:03 -0500

    Namespace change for product_import model

--- app/controllers/spree/admin/product_imports_controller.rb
+++ app/controllers/spree/admin/product_imports_controller.rb
@@ -8,12 +8,12 @@ class Spree::Admin::ProductImportsController < Spree::Admin::BaseController
   end
   
   def new
-    @product_import = ProductImport.new
+    @product_import = Spree::ProductImport.new
   end
   
   
   def create
-    @product_import = ProductImport.create(params[:product_import])
+    @product_import = Spree::ProductImport.create(params[:product_import])
     Delayed::Job.enqueue ImportProducts::ImportJob.new(@product_import, @current_user)
     flash[:notice] = t('product_import_processing')
     redirect_to :action => :new
